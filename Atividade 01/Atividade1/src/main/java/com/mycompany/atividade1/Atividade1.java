04bb43485896273f75c99fcb0dd6dcc3041c57b3
Otávio Teixeira <163035228+otFaria@users.noreply.github.com>
2024-03-15 22:35:44 -0300

    Delete Atividade 01/Atividade1 directory

--- Atividade 01/Atividade1/src/main/java/com/mycompany/atividade1/Atividade1.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.atividade1;
-import classes.Conssecionaria;
-import classes.Menu;
-public class Atividade1 {
-
-    public static void main(String[] args) {
-        Menu.implementacao();
-    }
-}
